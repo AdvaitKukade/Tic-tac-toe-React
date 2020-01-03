f6bdfc5f4f2c416b3fef137c0fe85532f99dd51b
AdvaitKukade <32466868+AdvaitKukade@users.noreply.github.com>
2020-01-03 18:05:15 +0530

    Added sudo fuser -k 3000/tcp multiple times.

--- npm.sh
+++ npm.sh
@@ -2,5 +2,9 @@ sudo chmod a+rwx /var/www/html/tttgame/
 cd /var/www/html/tttgame/
 sudo npm i
 sudo fuser -k 3000/tcp
+sudo fuser -k 3000/tcp
+sudo fuser -k 3000/tcp
+sudo fuser -k 3000/tcp
+sudo fuser -k 3000/tcp
 sudo pm2 start --name app npm -- run start
 sudo pm2 logs
