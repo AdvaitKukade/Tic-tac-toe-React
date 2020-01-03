ad80f9fe32742c876123f324c6e9e4b1e4670595
AdvaitKukade <32466868+AdvaitKukade@users.noreply.github.com>
2020-01-03 13:52:28 +0530

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -2,5 +2,5 @@ sudo chmod a+rwx /var/www/html/tttgame/
 cd /var/www/html/tttgame/
 sudo npm i
 sudo npm run build
-sudo pm2 start --name reactapp 
-PORT=3005 npm -- run start
+fuser -k 3000/tcp
+sudo pm2 start --name reactapp  npm -- run start
