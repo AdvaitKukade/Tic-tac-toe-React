48daf86dc8769187e542c2a0296c58520daf419e
AdvaitKukade <32466868+AdvaitKukade@users.noreply.github.com>
2020-01-06 16:38:46 +0530

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -3,6 +3,7 @@ cd /var/www/html/tttgame/
 sudo npm i 
 sudo npm run build
 cp ./build /var/www/html/
+sudo lsof -i:3000
 sudo kill -9 `sudo lsof -t -i:3000`
 sudo pm2 start --name app npm --run start
 sudo pm2 logs
