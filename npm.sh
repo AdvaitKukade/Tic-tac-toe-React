110d8d43467a7380b4bbab6c61649f2e215ef321
Advait Kukade <advaitkukade@gmail.com>
2020-01-07 19:58:46 +0530

    Deleting html files

--- npm.sh
+++ npm.sh
@@ -1,7 +1,9 @@
-sudo chmod a+rwx /var/www/html/tttgame/
-cd /var/www/html/tttgame/
-sudo kill -9 `sudo lsof -t -i:3000`
+sudo git pull origin master 
 sudo npm i 
 sudo npm run build
+sudo chmod a+rwx /var/www/html/tttgame/
+mv ./build /var/www/html/
+#cd /var/www/html/tttgame/
+sudo kill -9 `sudo lsof -t -i:3000`
 #sudo pm2 start --name app npm -- run start
 sudo npm -- run start
