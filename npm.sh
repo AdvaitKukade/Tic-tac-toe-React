c8bb1e84ac4c1a2068aec61335d504db0a6e4587
AdvaitKukade <32466868+AdvaitKukade@users.noreply.github.com>
2020-01-08 12:50:33 +0530

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,9 +1,11 @@
+cd /ReactApp
 sudo git pull origin master 
 sudo npm i 
 sudo npm run build
 sudo chmod a+rwx /var/www/html/tttgame/
 mv ./build /var/www/html/
 #cd /var/www/html/tttgame/
+cd /var/www/html/
 sudo kill -9 `sudo lsof -t -i:3000`
-#sudo pm2 start --name app npm -- run start
-sudo npm -- run start
+sudo pm2 start --name app npm -- run start
+#udo npm -- run startssh -i "Advait-KeyPair.pem" root@ec2-15-206-125-44.ap-south-1.compute.amazonaws.com
