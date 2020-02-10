1216ba1a2be0abd41ea5363e8d0346d5105a3bec
AdvaitKukade <32466868+AdvaitKukade@users.noreply.github.com>
2020-02-10 20:17:42 +0530

    Update script.sh

--- tttgame/script.sh
+++ tttgame/script.sh
@@ -1,4 +1,5 @@
-sudo apt-get install -y npm 
-sudo npm i --save
-sudo npm run build 
-sudo npm start 
+usermod -a -G sudo jenkins
+apt-get install -y npm 
+npm i --save
+npm run build 
+npm run start
